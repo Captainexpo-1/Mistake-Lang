629e91af8f8e2976f8e6bed6d0f433e1a622c5a4
Captainexpo-1 <captainexpo@gmail.com>
2025-01-10 17:10:51 -0500

    Accidentally forgot to commit some files :skull:

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="mistake-lang",
-    version="0.1.0",
+    version="0.1.1",
     author="Elliot",
     author_email="captainexpo@gmail.com",
     description="A Mistake",
